17129b9e3e66c345947fb62cbcf24163b57f80cf
Yaroslav <59771733+keyoneinc@users.noreply.github.com>
2024-07-17 23:47:37 +0100

    created Request class, and added it's defenition

---
+++ hw38/Request.cpp
@@ -0,0 +1,45 @@
+#include "Request.h"
+
+Request::Request(string destination, string passenger_name, string departure_date)
+{
+    this->request_id = ++counter;
+    this->destination = destination;
+    this->passenger_name = passenger_name;
+    this->departure_date = departure_date;
+
+}
+
+int Request::getRequestID() const
+{
+    return request_id;
+}
+
+void Request::setDestination(string destination)
+{
+    this->destination = destination;
+}
+
+void Request::setPassengerName(string passenger_name)
+{
+    this->passenger_name = passenger_name;
+}
+
+void Request::setDepatureDate(string depature_date)
+{
+    this->departure_date = depature_date;
+}
+
+string Request::getDestination() const
+{
+    return destination;
+}
+
+string Request::getPassengerName() const
+{
+    return passenger_name;
+}
+
+string Request::getDepartureDate() const
+{
+    return departure_date;
+}
---
+++ hw38/Request.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Request
+{
+private:
+    int counter;
+    int request_id;
+    string destination;
+    string passenger_name;
+    string departure_date;
+public:
+    Request(string destination, string passenger_name, string departure_date);
+
+    int getRequestID() const;
+
+    void setDestination(string destination);
+    void setPassengerName(string passenger_name);
+    void setDepatureDate(string depature_date);
+    
+    string getDestination() const;
+    string getPassengerName() const;
+    string getDepartureDate() const;
+
+};
+
